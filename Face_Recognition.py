2910ef471b23a4c874e23abf51f35e1b607ddc24
izzyyyt <isseysummer76@gmail.com>
2025-07-15 16:28:08 +0100

    Face change for Face_recognition.py

--- Face_Recognition.py
+++ Face_Recognition.py
@@ -1,7 +1,8 @@
+"""Recognises who appears in photos"""
 import face_recognition
 
 known_image = face_recognition.load_image_file("girl.jpeg")
-unknown_image = face_recognition.load_image_file("cat.jpeg")
+unknown_image = face_recognition.load_image_file("unknown.jpeg")
 
 girl_encoding = face_recognition.face_encodings(known_image)[0]
 unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
