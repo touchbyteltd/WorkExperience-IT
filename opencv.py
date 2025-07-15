23239422500cd4e3b2669834b3559fd93f54dbdb
izzyyyt <isseysummer76@gmail.com>
2025-07-15 14:05:53 +0100

    git ignore

--- opencv.py
+++ opencv.py
@@ -2,9 +2,10 @@
 import cv2
 
 #load image
-img = cv2.imread("image.jpeg",cv2.IMREAD_COLOR)
+img = cv2.imread("girl.jpeg",cv2.IMREAD_COLOR)
 
 #display image
 cv2.imshow("Image", img )
 
 cv2.waitKey(0)
+
