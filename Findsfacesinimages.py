566522d60b66547c1aa1627fdf6cc2ae9c6c33c1
izzyyyt <isseysummer76@gmail.com>
2025-07-16 16:03:36 +0100

    Detecting a face in an image and puting a rectange around it, note: I had to use OpenCV, Haar Cascade detection instead of dlib face_detection because of the errors with installing face_recognition

---
+++ Findsfacesinimages.py
@@ -0,0 +1,21 @@
+"""Finds a face in an image"""
+import cv2
+
+# loads image
+img = cv2.imread("girl2.jpeg")
+
+# converts to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# loads Haar cascade for face detection
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+# detects the faces with positional arguments
+faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+
+# puts a rectangle around face
+for (x, y, w,h) in faces:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+cv2.imshow("detected faces", img)
+cv2.waitKey(0)
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # WorkExperience-IT
+## Facial Recognition 
+The aims of this project are to recognises and detect faces from python in real time.
+## Installation
+### Requirements
+Python 3.1 and Linux
+You need to have dlib already installed as it is much more accurate compared to OpenCV, however you may come across errors installing dlibs face_recognition. 
