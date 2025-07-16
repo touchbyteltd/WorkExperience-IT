6e0ba25541e09da719eba4dd4d70a39c3337d951
izzyyyt <isseysummer76@gmail.com>
2025-07-16 10:54:23 +0100

    Displaying a single frame from the USB webcamera

---
+++ Displaying_a_face.py
@@ -0,0 +1,19 @@
+""""Displays feed from camera"""
+import cv2
+# Opens USB camera
+cap = cv2.VideoCapture(0)
+
+# Checks if the camera has opened successfully
+if not cap.isOpened():
+    print("Could not open camera")
+    exit()
+
+# Reads a single frame
+ret, frame = cap.read()
+
+# Shows the image if the frame is corrrectly read
+if ret:
+    cv2.imshow("captured Frame", frame)
+    cv2.waitKey(0)
+else:
+    print("Could not read frame")
